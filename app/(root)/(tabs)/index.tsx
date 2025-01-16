57cc1006efbb1a45fbe200ca7f5f986a26d24a00
Vinícius Santos <vinicius.ssantos.dev@gmail.com>
2025-01-16 01:44:45 -0300

    Add initial project setup with NativeWind integration and basic screens

---
+++ app/(root)/(tabs)/index.tsx
@@ -0,0 +1,20 @@
+import { Link } from "expo-router";
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text className="my-10 font-rubik text-3xl">Welcome to Re:Home</Text>
+      <Link href="/sign-in">Sign In</Link>
+      <Link href="/explore">Explore</Link>
+      <Link href="/profile">Profile</Link>
+      <Link href="/properties/1">Property</Link>
+    </View>
+  );
+}
