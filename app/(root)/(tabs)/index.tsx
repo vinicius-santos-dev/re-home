52c4d6f0e5de765b6370a4c87d52f4ba010b733c
Vinícius Santos <vinicius.ssantos.dev@gmail.com>
2025-01-17 01:51:26 -0300

    feat(profile, tabs): Add profile screen with settings and logout functionality; implement tab layout for navigation

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -11,10 +11,6 @@ export default function Index() {
       }}
     >
       <Text className="my-10 font-rubik text-3xl">Welcome to Re:Home</Text>
-      <Link href="/sign-in">Sign In</Link>
-      <Link href="/explore">Explore</Link>
-      <Link href="/profile">Profile</Link>
-      <Link href="/properties/1">Property</Link>
     </View>
   );
 }
